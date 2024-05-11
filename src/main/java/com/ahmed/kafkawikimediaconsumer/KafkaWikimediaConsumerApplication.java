cadb697fa199abc29eb56a4fbcc3f3deec29834d
AhmedI99 <ahmedibrahim5182@gmail.com>
2024-05-12 01:36:39 +0300

    Simple kafka demo ( consumer )

---
+++ src/main/java/com/ahmed/kafkawikimediaconsumer/KafkaWikimediaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.ahmed.kafkawikimediaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWikimediaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaWikimediaConsumerApplication.class, args);
+    }
+
+}
